9d99f32ecbbd6f401489ac9b786a6a3f0f65a99f
Brian Lee <clee689@my.bcit.ca>
2025-01-29 18:14:12 -0800

    Add Part C change

--- 3/server.js
+++ 3/server.js
@@ -1,31 +1,72 @@
+//Code is assisted with ChatGBT
+
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
+const path = require('path');
 const { MESSAGE } = require('./lang/en/en.js');
-const date = require('./modules/utils.js')
+const date = require('./modules/utils.js');
 
 class Routes {
   constructor() {
-    this.routes = {};
+    this.routes = new Map();
   }
 
   addRoute(path, handler) {
-    this.routes[path] = handler;
+    const regex = new RegExp(path.replace('*', '(.*)')); 
+    this.routes.set(regex, handler); 
   }
 
-  getHandler(path) {
-    return this.routes[path] || null; 
+  getHandler(urlPath) {
+    for (const [regex, handler] of this.routes) {
+      const match = urlPath.match(regex);
+      if (match) {
+        return { handler, params: match.slice(1) };
+      }
+    }
+    return null; 
   }
 }
 
 class RouteHandler {
   greet = (req, res) => {
     const parsedUrl = url.parse(req.url, true);
-    const name = parsedUrl.query.name|| "Guest";
-    const currentDate = date.getDate()
+    const name = parsedUrl.query.name || "Guest";
+    const currentDate = date.getDate();
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(`<p style="color: blue;">${MESSAGE.GREET(name, currentDate)}</p>`);
     res.end();
-  }
+  };
+
+  writeFile = (req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const text = parsedUrl.query.text || "";
+    const filePath = path.join(__dirname, 'file.txt');
+    fs.appendFile(filePath, text + '\n', (err) => { 
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Error appending to file');
+        console.error(err);
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Text appended to file successfully');
+    });
+  };
+
+  readFile = (req, res, fileName) => {
+    const filePath = path.join(__dirname, fileName);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end(`${fileName} not found`);
+        console.error(err);
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(data);
+    });
+  };
 }
 
 class ApiServer {
@@ -41,15 +82,18 @@ class ApiServer {
   start() {
     const server = http.createServer((req, res) => {
       const parsedUrl = url.parse(req.url, true);
-      const routeHandler = this.routes.getHandler(parsedUrl.pathname); // Get route handler from Routes class
+      const routeHandler = this.routes.getHandler(parsedUrl.pathname);
+
       if (routeHandler) {
-        routeHandler(req, res); 
+        const { handler, params } = routeHandler;
+        handler(req, res, ...params); 
       } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.write('404 Not Found');
         res.end();
       }
     });
+
     server.listen(this.port, () => {
       console.log(`Server running at http://localhost:${this.port}`);
     });
@@ -58,4 +102,6 @@ class ApiServer {
 
 const apiServer = new ApiServer(8080);
 apiServer.addRoute('/COMP4537/labs/3/getDate/', new RouteHandler().greet);
+apiServer.addRoute('/COMP4537/labs/3/writeFile/', new RouteHandler().writeFile);
+apiServer.addRoute('/COMP4537/labs/3/readFile/(.*)', new RouteHandler().readFile);
 apiServer.start();
